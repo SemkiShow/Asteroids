0ab17c055a705ce8a61c58dfe339a787f932b87b
SemkiShow <mgdeveloper123@gmail.com>
2026-05-15 10:05:33 +0200

    Add a simple camera

---
+++ camera.py
@@ -0,0 +1,48 @@
+import os
+
+
+class Camera:
+    position = (0, 0)
+    buf = []
+    last_terminal_size = (0, 0)
+
+    def __init__(self):
+        self.clear()
+
+    def get_terminal_size(self):
+        return os.get_terminal_size()
+
+    def clear(self):
+        terminal_size = self.get_terminal_size()
+
+        # Use fast terminal clear if the size hasn't changed
+        if self.last_terminal_size == terminal_size:
+            print(f"\033[{terminal_size.lines}A\033[{terminal_size.columns}D")
+        else:
+            print("\033[H\033[J", end="")
+
+        # Reset the internal buffer
+        self.buf = [
+            [" " for x in range(terminal_size.columns)] for y in range(terminal_size.lines - 1)
+        ]
+
+    def draw_char(self, x: int, y: int, val: str):
+        # Calculate the offset positions
+        nx = x + self.position[0]
+        ny = y + self.position[1]
+        terminal_size = self.get_terminal_size()
+
+        # Ignore pixels that are out of screen
+        if ny < 0 or ny >= len(self.buf) or nx < 0 or nx >= len(self.buf[ny]):
+            return
+
+        self.buf[ny][nx] = val
+
+    def flush(self):
+        for row in self.buf:
+            for cell in row:
+                print(cell, end="")
+            print()
+
+
+camera = Camera()
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+from camera import camera
+import time
+import os
+
+if __name__ == "__main__":
+    # Enable ANSI codes on PowerShell and CMD
+    os.system("")
+
+    x = 0
+    while True:
+        camera.clear()
+        camera.draw_char(x, x, "\x1b[31mt\x1b[0m")
+        camera.flush()
+
+        x += 1
+        x %= len(camera.buf)
+        time.sleep(0.1)
